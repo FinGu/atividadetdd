68e35ddd59704fe520d5650dc4a8ad34d1aa850c
Fabio Crepaldi <fabiocrepaldi@airmail.cc>
2025-10-06 14:44:11 -0300

    refatorar

--- src/main/java/com/tdd/atividade/Franc.java
+++ src/main/java/com/tdd/atividade/Franc.java
@@ -1,11 +1,12 @@
 package com.tdd.atividade;
 
 class Franc extends Money{   
-    Franc(int amount) {      
-      this.amount= amount;
+    Franc(int amount, String currency) {      
+      this.amount = amount;
+      this.currency = currency;
     }					
 
     Money times(int multiplier){
-       return new Franc(amount * multiplier);					
+       return Money.franc(amount * multiplier);					
     }   
 }
